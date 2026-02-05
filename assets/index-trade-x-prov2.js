40ac59ba9000f6a503705101678a40bdb0980c0d
visertech12 <i.am.visertech@gmail.com>
2026-02-05 08:56:34 +0500

    Update index-trade-x-prov2.js

--- assets/index-trade-x-prov2.js
+++ assets/index-trade-x-prov2.js
@@ -28507,36 +28507,6 @@ function Wallets({setPageTitle: _e}) {
                     })]
                 })
             }),
-      jsxRuntimeExports.jsxs("div", {
-                                className: "text-end",
-                                children: [
-                                    jsxRuntimeExports.jsx("h1", {
-                                        className: "text-[12px] text-gray-300",
-                                        children: "Profit Balance"
-                                    }),
-                                    jsxRuntimeExports.jsxs("h1", {
-                                        className: "text-[24px] font-bold text-white",
-                                        children: [
-                                            rt ? showAmount(rt.profit_balances) : "0.00",
-                                            " ",
-                                            jsxRuntimeExports.jsx("span", {
-                                                className: "text-[12px]",
-                                                children: xt?.cur_text || ""
-                                            })
-                                        ]
-                                    }),
-                                    jsxRuntimeExports.jsxs("h1", {
-                                        className: "text-[10px] text-gray-400 flex items-center",
-                                        children: [
-                                            "USD ",
-                                            rt ? showAmount(rt.profit_balances) : "0.00",
-                                            xt?.cur_sym || ""
-                                        ]
-                                    })
-                                ]
-                            })
-                        ]
-                    }),
     
             
             jsxRuntimeExports.jsxs("div", {
@@ -28655,7 +28625,7 @@ function Wallets({setPageTitle: _e}) {
                     children: Array(10).fill(null).map( (pt, dt) => jsxRuntimeExports.jsx(WalletCoinsSkl, {}, dt))
                 })
             })]
-
+        })]
     })
 }
 function UserLogin() {
